1e5709045d9e3b7a6191f06a98b4d1482bbbc70d
Miguel-Ledesma <ledesmamiguel184@gmail.com>
2022-08-26 13:42:28 -0400

    trying to fix fader problem

--- main-site/javascript/fader.js
+++ main-site/javascript/fader.js
@@ -2,5 +2,5 @@ const temp = document.querySelector('.temp');
 document.addEventListener('DOMContentLoaded', (e)=> {
     setTimeout(() => {
         temp.classList.add('display-none');
-    }, 2000);
+    }, 1500);
 })
